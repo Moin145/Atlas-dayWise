b70ddd5bc582adf23ba2531b1e60869669b15071
Moin145 <111505690+Moin145@users.noreply.github.com>
2025-09-20 13:53:52 +0530

    Updated/added files in setup-days

---
+++ day20/EmployeeEmailService.java
@@ -0,0 +1,7 @@
+package PracticeSet.atlaslearnings.day20.SRPEmployee;
+
+public class EmployeeEmailService {
+    public void sendEmail(Employee employee) {
+        System.out.println("Sending email to " + employee.getEmail());
+    }
+}
---
+++ day20/EmployeeReportGenerator.java
@@ -0,0 +1,8 @@
+package PracticeSet.atlaslearnings.day20.SRPEmployee;
+
+public class EmployeeReportGenerator {
+    public void generatePdfReport(Employee employee) {
+
+        System.out.println("Generating PDF report for " + employee.getName());
+    }
+}
